18896ae32c661e9f3c2a02d4565ce57b8b7a76de
Jamie Hargreaves <41867515+Jamie3213@users.noreply.github.com>
2022-01-31 23:38:30 +0000

    Refactor tiingo.py

--- producer/tiingo.py
+++ producer/tiingo.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import gzip
 import json
 import re
@@ -5,8 +7,7 @@ import time
 import traceback
 from dataclasses import asdict
 from datetime import datetime
-from typing import List, Optional, Tuple
-from urllib import response
+from typing import List, Optional
 
 import boto3
 from botocore.exceptions import ClientError
@@ -21,18 +22,22 @@ logger = helpers.create_logger("tiingo")
 
 
 @dataclass
-class _TradeUpdate:
-    """Trade update from the Tiingo crypto API.
+class TradeData:
+    """Trade data extracted from the API response message.
 
-    Attributes:
-        ticker (str): The asset ticker.
-        date (str): The trade timestamp in the form "%Y-%m-%dT%H:%M:%S.%fZ".
-        price (str): The price the trade was executed at.
-        processed_at (str): Application timestamp in the form "%Y-%m-%dT%H:%M:%SZ".
+    Attibutes:
+        ticker (str): The ticker for the crypto asset.
+        date (str): The trade execution timestamp.
+        exchange (str): The exchange on which the trade took place.
+        size (float): The volume done at the last price.
+        price (float): The price the trade was executed at.
+        processed_at (str): Timestamp when the record was processed by the application.
     """
 
     ticker: str
     date: str
+    exchange: str
+    size: float
     price: float
     processed_at: str
 
@@ -49,15 +54,111 @@ class _TradeUpdate:
             return timestamp
 
 
-@dataclass
-class TradeUpdateBatch:
+class TiingoSubscribeError(Exception):
+    pass
+
+
+class TiingoClient(WebSocket):
+    """Extends WebSocket methods to add Tiingo specific functionality for use
+    in connecting to Tiingo WSS APIs.
+
+    Attributes:
+      url (str): The API URL.
+      token (str): The API auth token.
+    """
+
+    def __init__(self, url: str, token: str):
+        """Inits the client and subscribes to the specified API endpoint.
+        
+        Attributes:
+            url (str): The API URL.
+            token (str): The API auth token that should be used to subscribe to the API.
+        """
+
+        self._token: str = token
+        self._url: str = url
+
+        self.connect(self._url)
+        self._subscribe()
+
+    def _subscribe(self) -> None:
+        # Subscribes to the API using the instance auth token.
+        subscribe = {
+            "eventName": "subscribe",
+            "authorization": self.token,
+            "eventData": {"thresholdLevel": 5},
+        }
+
+        subscription_message = json.dumps(subscribe)
+        self.send(subscription_message)
+
+        # Get the subscription response.
+        record = json.loads(self.recv())
+        message_type = record["messageType"]
+        
+        if message_type != "I":
+            raise TiingoSubscribeError(
+                f"Expected message type 'I' but found '{message_type}'."
+            )
+
+        response_message = record["response"]["code"]
+        response_code = record["response"]["code"]
+        if response_code != 200:
+            raise TiingoSubscribeError(
+                f"Failed with error code {response_code} and message '{response_message}'."
+            )
+
+    def next_trade(self) -> TradeData:
+        """Returns the next trade message from the API."""
+        record = json.loads(self.recv())
+        message_type = record["messageType"]
+
+        if message_type == "H":
+            self.next_trade()
+        
+        _, ticker, date, exchange, size, price = record["data"]
+
+        output_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+        api_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+
+        formatted_date = self._process_date(date, api_format, output_format)
+        now = datetime.strftime(datetime.utcnow(), output_format)
+
+        return TradeData(
+            ticker,
+            formatted_date,
+            exchange,
+            size,
+            price,
+            now
+        )
+
+    @staticmethod
+    def _process_date(date_as_string: str, format_in: str, format_out: str) -> str:
+        # Trade timestamps returned from the Tiingo API have a standard format
+        # except in instances where the microsecond, "%f", part is zero, in
+        # which case it is omitted. In this case, the method adds the correct
+        # a zero "%f" part to the timestamp.
+        try:
+            date_as_datetime = datetime.strptime(date_as_string, format_in)
+        except ValueError:
+            first_part = date_as_string[:19]
+            last_part = date_as_string[19:]
+            f_part = f".{6 * '0'}"
+            date_as_datetime = datetime.strptime(f"{first_part}{f_part}{last_part}", format_in)
+
+        return datetime.strftime(date_as_datetime, format_out)
+
+
+class TradeBatch:
     """Holds batches of trade updates.
 
     Attributes:
         batch (List[_TradeUpdate]): A list of APi response messages.
     """
 
-    batch: List[_TradeUpdate]
+    def __init__(self, batch):
+        self.batch: List[TradeData] = batch
 
     def put_to_stream(self, stream: str) -> None:
         """Puts records to the Kinesis Firehose Stream. If a service error occurs,
@@ -105,121 +206,3 @@ class TradeUpdateBatch:
         firehose_client = boto3.client("firehose")
         put_record = {"Data": record}
         firehose_client.put_record(DeliveryStreamName=stream, Record=put_record)
-
-
-class TiingoSubscribeError(Exception):
-    pass
-
-
-class TiingoSession(WebSocket):
-    """A websocket session connecting to a Tiingo WSS API.
-
-    Attributes:
-      url (str): The API URL.
-      token (str): The API auth token.
-    """
-
-    def __init__(self, url: str, token: str) -> None:
-        """Inits TiingoSession with the specified API auth token."""
-        self.url = url
-        self.token = token
-        self.ws = self._subscribe_and_flush()
-
-    def _subscribe_and_flush(self) -> WebSocket:
-        # Subscribes to the crypto API and flushes initial responses.
-        ws = WebSocket()
-        ws.connect(self.url)
-
-        subscribe = {
-            "eventName": "subscribe",
-            "authorization": self.token,
-            "eventData": {"thresholdLevel": 5},
-        }
-
-        message = json.dumps(subscribe)
-        ws.send(message)
-
-        # Get the first API message
-        response = json.loads(ws.recv())["response"]
-        response_code = response["code"]
-        response_message = response["message"]
-
-        if response_code != 200:
-            raise TiingoSubscribeError(
-                f"Failed with error code {response_code} and message '{response_message}'."
-            )
-
-        return ws
-
-    def get_batch(self, size: int) -> TradeUpdateBatch:
-        """Creates a batch of trade update messages.
-
-        Attributes:
-            size (int): The size of the batch.
-
-        Returns:
-            TradeUpdateBatch: List of messages from the API.
-        """
-        batch = [self._next_trade() for _ in range(size)]
-        return TradeUpdateBatch(batch)
-
-    def _next_trade(self) -> _TradeUpdate:
-        # Returns the next trade from the API.
-        data, update_type = self._get_response()
-
-        # Only return trades, not top-of-book quotes
-        while update_type != "T":
-            data, update_type = self._get_response()
-
-        return self._process_response_data(data)
-
-    def _get_response(self) -> Tuple[List[str], str]:
-        # Returns the next API response and the update type.
-        record, response_type = self._filter_response_type()
-
-        while response_type != "A":
-            record, response_type = self._filter_response_type()
-
-        data = record["data"]
-        update_type = data[0]
-
-        return (data, update_type)
-
-    def _filter_response_type(self) -> Tuple[dict, str]:
-        # Extract the message type from the API response and return it
-        # along with the response itself.
-        response = self.ws.recv()
-        record = json.loads(response)
-        response_type = record["messageType"]
-
-        return (record, response_type)
-
-    def _process_response_data(self, data: List[str]) -> _TradeUpdate:
-        # Creates a TradeUpdateMessage dataclass from a data array.
-        ticker = data[1]
-        date = data[2]
-        price = data[5]
-
-        output_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-        api_format = "%Y-%m-%dT%H:%M:%S.%f%z"
-
-        formatted_date = self._process_date(date, api_format, output_format)
-        now = datetime.strftime(datetime.utcnow(), output_format)
-
-        return _TradeUpdate(ticker, formatted_date, float(price), now)
-
-    @staticmethod
-    def _process_date(date_as_string: str, format_in: str, format_out: str) -> str:
-        # Trade timestamps returned from the Tiingo API have a standard format
-        # except in instances where the microsecond, "%f", part is zero, in
-        # which case it is omitted. In this case, the method adds the correct
-        # a zero "%f" part to the timestamp.
-        try:
-            date_as_datetime = datetime.strptime(date_as_string, format_in)
-        except ValueError:
-            first_part = date_as_string[:19]
-            last_part = date_as_string[19:]
-            f_part = f".{6 * '0'}"
-            date_as_datetime = datetime.strptime(f"{first_part}{f_part}{last_part}", format_in)
-
-        return datetime.strftime(date_as_datetime, format_out)
