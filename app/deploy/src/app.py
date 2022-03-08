871bfd2f5ab07a5a211bd853928775348241b2d3
Jamie Hargreaves <41867515+Jamie3213@users.noreply.github.com>
2022-03-08 10:05:31 +0000

    Update CDK app.

--- app/deploy/src/app.py
+++ app/deploy/src/app.py
@@ -1,28 +1,12 @@
-#!/usr/bin/env python3
-import os
-
-import aws_cdk as cdk
-
-from deploy.deploy_stack import DeployStack
+from aws_cdk import App, Stack
+from aws_cdk import aws_kinesisfirehose as firehose
+from constructs import Construct
 
 
-app = cdk.App()
-DeployStack(app, "DeployStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+class CryptoStreamingStack(Stack):
+    def __init__(self, scope: Construct, id: str) -> None:
+        super().__init__(scope, id)
+        
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
-app.synth()
+if __name__ == "__main__":
+    app = App()
